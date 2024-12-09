67c5108df92bc6bf1ed4916cb4b85980c2b06781
Shiyun Lu <lushiyun@outlook.com>
2024-12-09 05:04:30 -0500

    try more

--- apps/web/db/database.ts
+++ apps/web/db/database.ts
@@ -5,6 +5,7 @@ import { Kysely, PostgresDialect } from "kysely";
 const dialect = new PostgresDialect({
   pool: new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: true,
   }),
 });
 
