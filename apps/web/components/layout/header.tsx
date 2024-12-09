6eeeb72e5ce565d6267317cb809a0b38a82bd59e
Shiyun Lu <lushiyun@outlook.com>
2024-12-09 03:56:13 -0500

    work

--- apps/web/components/layout/header.tsx
+++ apps/web/components/layout/header.tsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <header className="bg-bg sticky top-0 z-10 flex items-center justify-between py-4">
       <Tooltip content="Programmatic color css variables generator for multi-theme platforms">
-        <Link href="/">
+        <Link href="/docs">
           <h1 className="font-mono text-lg">coloris.js</h1>
         </Link>
       </Tooltip>
