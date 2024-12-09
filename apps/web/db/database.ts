7d445c02d16a29f06c1e6c064152f7468d0e9c33
Shiyun Lu <lushiyun@outlook.com>
2024-12-09 05:10:40 -0500

    try again

--- apps/web/db/database.ts
+++ apps/web/db/database.ts
@@ -1,12 +1,11 @@
 import { Database } from "./types"; // this is the Database interface we defined earlier
-import { Pool } from "pg";
-import { Kysely, PostgresDialect } from "kysely";
+import { Kysely } from "kysely";
+import { NeonDialect } from "kysely-neon";
+import ws from "ws";
 
-const dialect = new PostgresDialect({
-  pool: new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-  }),
+const dialect = new NeonDialect({
+  connectionString: process.env.DATABASE_URL,
+  webSocketConstructor: ws,
 });
 
 // Database interface is passed to Kysely's constructor, and from now on, Kysely
